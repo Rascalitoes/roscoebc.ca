7996d06aabb43302f23ff40a5010120eee6e2134
Rascalitoes <55996478+Rascalitoes@users.noreply.github.com>
2026-06-19 21:35:16 -0400

    v2 first draft

--- frontend/src/components/PortfolioCard.tsx
+++ frontend/src/components/PortfolioCard.tsx
@@ -26,35 +26,44 @@ const PortfolioCard: React.FC<CardProps> = ({ title, stack, tags, description, i
     // let direction = horizontal ? "w" : "h";
 
     return (
-        <div className={`portfolio-card-${horizontal ? "w" : "h"} bg-stone-50 rounded-2xl shadow-sm text-black m-10 flex-col" ${className}`}>
-            <div className="bg-orange-400 rounded-t-2xl border-b border-black py-2">
-                <h3 className="text-4xl font-bold ml-5">{title}</h3>
-            </div>
-            <div className='content p-5 flex-1'>
-                <div className='tagWrapper mx-2 mt-0 mb-4 space-y-2'>
-                    <div className='stack space-x-1'>
-                        {/* <span className='code font-bold'>Tech Stack:</span> */}
-                        <ChipList list={stack ? stack : []}></ChipList>
-                    </div>
-                    {/* <div className='tags space-x-1'>
+        <div className={`portfolio-card-${horizontal ? "w" : "h"} bg-stone-50 rounded-2xl shadow-sm text-black m-10 flex flex-row" ${className}`}>
+            <div className='left-card w-[60%] flex flex-col'>
+                <div className="bg-orange-400 rounded-tl-2xl border-b border-black py-2">
+                    <h3 className="text-4xl font-bold ml-5">{title}</h3>
+                    <div className='tagWrapper mx-2 mt-0 mb-4 space-y-2'>
+                        <div className='stack space-x-1'>
+                            {/* <span className='code font-bold'>Tech Stack:</span> */}
+                            <ChipList list={stack ? stack : []}></ChipList>
+                        </div>
+                        {/* <div className='tags space-x-1'>
                         <span className='code'>Other Tags:</span>
                         <ChipList list={tags}></ChipList>
                     </div> */}
+                    </div>
                 </div>
 
-
+                <div className="flex bg-stone-200">
+                        <img
+                            src={images}
+                            alt="Content"
+                            className={`${horizontal ? "h-64" : "w-full"} self-center object-cover rounded-2xl border border-black`}
+                        />
+                    </div>
+            </div>
+            <div className='content p-5 flex-1'>
                 <div className={`gap-6 flex flex-${horizontal ? "row" : "col"}`}>
                     {/* Image Placeholder */}
-                    <div className="shrink-0">
+                    {/* <div className="shrink-0">
                         <img
                             src={images}
                             alt="Content"
                             className={`${horizontal ? "h-64" : "w-full"} object-cover rounded-2xl border border-black`}
                         />
-                    </div>
+                    </div> */}
 
                     {/* Description */}
                     <div className="flex-1 flex flex-col justify-center">
+                        <h4 className='text-neutral-600 mt-8 mb-4'>About</h4>
                         <p className=" text-gray-900 leading-relaxed">
                             {description}
                         </p>
